4a33b121ef34380a57db468eb9cc6dfbb1727f96
EarthlySkies <earthlyskies@pm.me>
2025-11-15 00:30:15 +0200

    Update main.py structure

--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@
 ## The "actual stuff" is done in the agents and components themselves.
 
 import os
+import multiprocessing
 
 ## Start agents and workers here
+
 ## We'll need to fork the agents into their own processes as they'll be making
 ## blocking operations. If we try to run everything in a single process, we'll
 ## be blocking ourselves.
@@ -11,5 +13,6 @@ import os
 ## Loop
 
   ## Poll agents here
+  ## Use queues, as the data transfers is only one way: child -> parent, agent -> controller
 
   ## Pass sum of agent desires to plan executor here
