c3a35bf886c48e8f28341aac1064bc36da763ed0
EarthlySkies <earthlyskies@pm.me>
2025-11-14 22:28:16 +0200

    Expanded electricity tracker job

--- src/electricity-tracker-agent.py
+++ src/electricity-tracker-agent.py
@@ -1,3 +1,7 @@
 ## This agent keeps track of changing electicity prices
 ## Outputs desire based on how much electricity costs at a given moment
 ## Desire output is between -1.0 to +1.0
+
+## Electricity prices for a given next day are generally available at 14:30
+## on a given day. We can use this data to predict future prices and optimize
+## our pumping to times of low cost.
