c53da38f6d3c4538015bfa924b4551e08b30bce8
EarthlySkies <earthlyskies@pm.me>
2025-11-14 22:48:24 +0200

    Update tunnel emptier job description

--- src/tunnel-emptier.py
+++ src/tunnel-emptier.py
@@ -1,7 +1,8 @@
 ## This component ensures that the water storage reaches level L1 at least
 ## every two days for compliance reasons.
 
-## L1 storage **must** be emptied to L1 min every 2 days.
+## L1 storage **must** be emptied to L1 min every 2 days. It can be emptied
+## more frequently if desired, but minumum is every 48 hours.
 
 ## The job of this component is to figure out the optimum timing for emptying
 ## the storage tank to its minimum level. This is for compliance reasons, as
