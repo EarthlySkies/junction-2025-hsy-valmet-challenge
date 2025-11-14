204a587296eed51342446566bc36ebc0b63e02bc
EarthlySkies <earthlyskies@pm.me>
2025-11-14 22:59:22 +0200

    Clarified storage tracker agent job description

--- src/storage-tracker-agent.py
+++ src/storage-tracker-agent.py
@@ -1,3 +1,6 @@
 ## This agent keeps track of the storage water level
 ## Output more desire the more storage is used
 ## Output is between 0.0 to +1.0
+
+## The job of this agent to prevent the storage from overflowing. Thus, it
+## simply increases its desire as the water level rises in the storage.
