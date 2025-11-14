cc7846456ebc5a0bec8d2a69d55c53fb015be971
EarthlySkies <earthlyskies@pm.me>
2025-11-15 00:37:12 +0200

    safety compliance enforcer structure updates

--- src/safety-compliance-enforcer.py
+++ src/safety-compliance-enforcer.py
@@ -1,2 +1,9 @@
 ## This component ensures that the system operates within predefined safety
 ## margins.
+
+## Basically, we need to make sure at least one pump is running at all times
+## and that our storage tunnel (L1) is never filled beyond full capacity.
+
+## If the storage capacity is exceeded beyond a pre-defined safe operating
+## limit, this enforcer will ensure more pumps are activated to prevent
+## overflows and safety limits.
