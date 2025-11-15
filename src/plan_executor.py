a029af74012cfe7ba01ef963367098e994827c85
EarthlySkies <earthlyskies@pm.me>
2025-11-15 16:10:16 +0200

    fixed stuff

--- src/plan_executor.py
+++ src/plan_executor.py
@@ -8,30 +8,11 @@
 
 import time
 import multiprocessing
-import request
-from typing import Dict, List
 
 def plan_executor(watcher_data_socket):
-    PUMP_IDS: Dict[str, float]={
-        "Pump 1.1": 0,
-        "Pump 1.2": 0,
-        "Pump 1.3": 0,
-        "Pump 1.4": 0,
-        "Pump 2.1": 0,
-        "Pump 2.2": 0,
-        "Pump 2.3": 0,
-        "Pump 2.4": 0,
-    }
     while True:
         try:
             print(watcher_data_socket.recv())
         except EOFError:
             time.sleep(1)
         time.sleep(2)
-        # 
-        current_pump_statuses = requests.get("http://127.0.0.1:8000/pump-statuses")
-        for id in PUMP_IDS:
-            if current_pump_statuses[id] != 0:
-                PUMP_IDS[id] += 0.25
-        
-        
