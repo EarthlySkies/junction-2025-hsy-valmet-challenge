5a528224896dec05adfc6f04ea176cdeb87c1fdd
Šimon Mravec <simon.mravec@gmail.com>
2025-11-15 04:33:46 +0200

    Simulation Server

--- src/main.py
+++ src/main.py
@@ -10,3 +10,27 @@ import os
 ## Poll agents here (loop)
 
 ## Pass sum of agent desires to plan executor
+
+
+### Interacting with the simulation 
+
+import requests
+
+outflow = {
+    "outflow": 1.0,
+}
+
+#each 15 minutes yo
+pass_15_min = requests.post("http://127.0.0.1:8000/outflow", json=outflow, timeout=5)
+
+current_water_level = requests.get("http://127.0.0.1:8000/water-level")
+
+#rain in 15 min
+next_rain = requests.get("http://127.0.0.1:8000/next-rain")
+
+#elec
+next_rain = requests.get("http://127.0.0.1:8000/current-elec-price")
+
+
+
+
